687e0756fc743c6a86dcda4bc8299eb1587aa2eb
Brandon K <bkovaly@microsoft.com>
2015-07-27 11:37:55 -0700

    Initial commit

---
+++ ytjeopardy/ytjeopardy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ytjeopardy
+//
+//  Created by Brandon K on 7/27/15.
+//  Copyright (c) 2015 bk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ytjeopardy/ytjeopardy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ytjeopardy
+//
+//  Created by Brandon K on 7/27/15.
+//  Copyright (c) 2015 bk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
