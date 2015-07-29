e26634af62cd2d2e4a8cf9303998fb3571641379
Yarelly Janet Gomez <v-yagome@microsoft.com>
2015-07-29 11:26:29 -0700

    made some sweet ui changes to the question screen

---
+++ ytjeopardy/ytjeopardy/QuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuetionViewController.h
+//  ytjeopardy
+//
+//  Created by Yarelly Janet Gomez on 7/29/15.
+//  Copyright (c) 2015 bk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionViewController : UIViewController
+
+@end
