fc98e062b8fc38b80a3af47747ac09b07815897b
Poonam Hattangady <poonamh@microsoft.com>
2015-07-31 03:26:32 -0700

    Added jeopardy game controller (state machine) and tests

---
+++ ytjeopardy/ytjeopardy/JeopardyManager.h
@@ -0,0 +1,39 @@
+//
+//  JeopardyManager.h
+//  ytjeopardy
+//
+//  Created by Poonam Hattangady on 7/31/15.
+//  Copyright (c) 2015 bk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Team.h"
+#import "Question.h"
+
+@interface JeopardyManager : NSObject
+
+@property Team *team1;
+@property Team *team2;
+@property Team *team3;
+
+#pragma mark - Game Management 
+
+// Start the game. Picks one of the teams to go first.
+-(void)startGameWithTeam1:(NSString *)name1 team2:(NSString *)name2 team3:(NSString *)name3 questionFileName:(NSString *)fileName;
+
+// Returns the question for the category and assigns a team to answer it based on team state.
+- (Question *)startQuestionForCategory:(NSString*)category index:(NSInteger)index;
+
+// Returns YES when it's time to move to the next question:
+//  - when the question was answered correctly OR
+//  - when no team answered the question correctly
+// Returns NO when another team gets to answer the question.
+-(BOOL)questionAnswered:(BOOL)answeredCorrectly;
+
+-(BOOL)checkGameOver;
+
+#pragma mark - Singleton
+
++ (instancetype)sharedInstance;
+
+@end
