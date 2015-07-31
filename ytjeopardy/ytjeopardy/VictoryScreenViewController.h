b2555ef4587ae1bc0b37dd2e38de95e2cbc7c0a0
Poonam Hattangady <poonamh@microsoft.com>
2015-07-31 07:23:02 -0700

    Adding placeholder for Victory screen.

---
+++ ytjeopardy/ytjeopardy/VictoryScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  VictoryScreenViewController.h
+//  ytjeopardy
+//
+//  Created by Poonam Hattangady on 7/31/15.
+//  Copyright (c) 2015 bk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VictoryScreenViewController : UIViewController
+
+@end
